f28ddc8fd8f3a7271f099fc15fd578f641d14627
Rajehdidntwakeup <rajeh3010@gmail.com>
2025-09-14 14:08:29 +0200

    add Mock test

--- src/test/java/test/servicea/ServiceTest.java
+++ src/test/java/test/servicea/ServiceTest.java
@@ -1,30 +1,33 @@
 package test.servicea;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 import test.servicea.controller.CatController;
 import test.servicea.domain.Cat;
 import test.servicea.domain.CatDto;
 import test.servicea.repository.CatRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class ServiceTest {
-
-
 
     @Mock
     public CatRepository catRepository;
 
     @InjectMocks
-    public CatController catController = new CatController(catRepository);
+    public CatController catController;
 
     @Test
     public void test() {
         CatDto catDto = new CatDto("name", "color", 1);
 
-        Mockito.when(catRepository.save(new Cat(catDto.getName(), catDto.getColor(), catDto.getAge()))).thenReturn(new Cat());
-        catController.saveCat(catDto);
+        Mockito.when(catRepository.save(Mockito.any(Cat.class))).thenReturn(new Cat());
+        ResponseEntity<String> response = catController.saveCat(catDto);
+        assert response.getStatusCode().is2xxSuccessful();
     }
 
 }
